a246c323c59736a59d693d10b2e276f3bf6230ce
dobbobalina2 <dobbobaline@gmail.com>
2026-02-02 15:24:21 -0500

    boom

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e27f29a85f05f494")
+/******/ 		__webpack_require__.h = () => ("2f419db0cfa25fdf")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,9 +21,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "px-6 py-3",
-        sm: "px-4 py-2.5 text-xs",
-        lg: "px-10 py-4 text-base",
+        default: "px-10 py-3",
+        sm: "px-8 py-2.5 text-xs",
+        lg: "px-8 py-4 text-base",
         icon: "h-10 w-10",
       },
     },
